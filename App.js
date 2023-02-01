b96a043bf14e1ae7db90ab5a39737cffa0d27338
jung woo Kwon <kwog5050@naver.com>
2023-02-01 18:29:49 +0900

    커밋

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View>
+            <ImageBackground source={require("./assets/main/22.jpg")} style={styles.bg}>
+                <Text style={styles.startButton}>새게임</Text>
+                <Text style={styles.startButton}>저장된 게임</Text>
+            </ImageBackground>
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    bg: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        height: '100%',
+    },
+    startButton: {
+        backgroundColor: '#aaa',
+        width: '80%',
+        textAlign: 'center',
+        lineHeight: 70,
+        marginTop: 5,
+        marginBottom: 5,
+        color: '#fff',
+        fontSize: 24,
+    }
+})
